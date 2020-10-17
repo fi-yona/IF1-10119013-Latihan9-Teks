f0392e4a34aa0c6838050bed22ca8777626842f0
FionaAvila <fionaavila15@gmail.com>
2020-10-17 13:58:25 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan9/teks/if110119013Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan9.teks;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+public class if110119013Latihan9Teks {
+
+    public static void main(String[] args) {
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        
+        c = 'A';
+        
+        System.out.println("Hasil" + newline + "Char :" + tab + "berisi " + c);
+    }
+    
+}
